ad62c5b0869a09245a2966f71bdc4449a2661e4e
Jonathan Palencia <strikeone88@rsthn.com>
2020-05-13 23:45:13 -0600

    Published: v1.0.3

--- deploy.js
+++ deploy.js
@@ -46,8 +46,10 @@ run('echo Published: v'+package.version+' >>messages.log')
 .then(r => run('git branch temporal'))
 .then(r => run('git checkout temporal'))
 
+.then(r => run('del .gitignore'))
 .then(r => run('del deploy.js'))
 .then(r => run('del README.md'))
+.then(r => run('del messages.log'))
 
 .then(r => run('git commit -a -m "Preparing for release: '+package.version+'"'))
 .then(r => run('git push origin temporal'))
@@ -56,7 +58,6 @@ run('echo Published: v'+package.version+' >>messages.log')
 .then(r => run('git checkout master'))
 .then(r => run('git branch -D temporal'))
 .then(r => run('git push origin --delete temporal'))
-.then(r => run('del messages.log'))
 //.then(r => run('git reset'))
 
 .then(() => {
